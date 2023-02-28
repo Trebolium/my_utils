ed8f33c7891ce50536c4ef49f933e5daf9fe8b36
BrendanOConnor <brendan242424@gmail.com>
2023-02-28 13:21:25 +0000

    added feature to optionally convert arrays as well as scalars

--- my_audio/midi.py
+++ my_audio/midi.py
@@ -1,10 +1,14 @@
 # from https://gist.github.com/YuxiUx/ef84328d95b10d0fcbf537de77b936cd
 from math import log2
+import numpy as np
 ref_freq = 440
 ref_midi = 69
 
 def freqToMidi(freq):
-    midi_note = 12 * log2(freq/ref_freq) + ref_midi
+    if type(freq) == np.ndarray:
+        midi_note = 12 * np.log2(freq/ref_freq) + ref_midi
+    else: 
+        midi_note = 12 * log2(freq/ref_freq) + ref_midi
     return midi_note
 
 def midiToFreq(midi_note):
